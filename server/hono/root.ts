d28411312fb9d273d66994cfdbcf731dd58d51af
Muharu <harunalrd.mail@gmail.com>
2024-10-31 14:11:34 +0700

    feat: add basic trpc server

--- server/hono/root.ts
+++ server/hono/root.ts
@@ -1,9 +1,19 @@
+import { trpcServer } from "@hono/trpc-server";
 import { Hono } from "hono";
+import { appRouter } from "server/trpc";
 
 const app = new Hono();
 
 export const apiRoutes = app.basePath("/api").get("/hello", (ctx) => {
   return ctx.json({ message: "Hello, World!" });
 });
+
+apiRoutes.use(
+  "/procedure/*",
+  trpcServer({
+    router: appRouter,
+    endpoint: "/api/procedure",
+  })
+);
 
 export type ApiRoutes = typeof apiRoutes;
