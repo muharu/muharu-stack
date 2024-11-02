91ad22e03de7ef9cf21cd520c798840b65bba070
Muharu <harunalrd.mail@gmail.com>
2024-11-02 16:02:15 +0700

    build: remove test pages

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,17 +1,9 @@
 import type { MetaFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
-import { buttonVariants } from "~/components/ui/button";
 
 export const meta: MetaFunction = () => {
   return [{ title: "Create Muharu Stack" }];
 };
 
-export default function Index() {
-  return (
-    <main className="flex h-screen flex-col items-center justify-center gap-y-5">
-      <Link to="/signup" className={buttonVariants()}>
-        Go to signup page
-      </Link>
-    </main>
-  );
+export default function IndexPage() {
+  return <main></main>;
 }
